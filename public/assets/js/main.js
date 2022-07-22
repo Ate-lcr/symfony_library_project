134111b1df92ba6a42535153f72a0309447756da
Antoine Leclaire <104761460+Ate-lcr@users.noreply.github.com>
2022-07-22 09:50:30 +0200

    add localhost stockage for DM

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,14 +1,24 @@
 const nightBtn=document.querySelector(".js-night");
 const body=document.querySelector(".js-body");
 
-console.log("hello there");
+
+// Is NightMode enabled in localstorage ?,
+// If yes, enabled it in the current page
+const nightMode  = localStorage.getItem('night-activated')=== 'true';
+if (nightMode) {
+    body.classList.add("night-activated");
+}
 
 nightBtn.addEventListener('click', function (){
     console.log("click");
 
+
     if(body.classList.contains("night-activated")){
         body.classList.remove("night-activated");
+        localStorage.removeItem('night-activated');
+
     }else{
         body.classList.add("night-activated");
+        localStorage.setItem('night-activated', 'true');
     }
 });
